5533cdfcb9d827071e47dc7abc5f93ef8ea4130b
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-10-26 13:45:25 -0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,1 +1,16 @@
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'your_username',
+    password: 'your_password',
+    database: 'employee_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+module.exports = connection;
